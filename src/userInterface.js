69cbb74b6f62b1cb00b8b361dc194789a024ca7c
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-03-08 15:44:02 -0500

    The slider now functions but I'd like to change it later

--- src/userInterface.js
+++ src/userInterface.js
@@ -6,22 +6,25 @@ window.jobApp ={};
 
 
 //looks for the value of the value of full-name
-  document.querySelector('[name=full-name]').addEventListener('blur', function checkLength(eventObj){
-      console.log("off the box!!!");
-      if (document.querySelector('[name=full-name]').value.length === 0){
-        console.log("You aint got not text!!!");
-          document.querySelector('[name=full-name]').parentNode.classList.add('has-error');
+  document.querySelector('#full-name').addEventListener('blur', function checkLength(eventObj){
+      // console.log("off the box!!!"); eventObj.target.value
+      if (document.querySelector('#full-name').value.length === 0){
+        // console.log("You aint got not text!!!");
+          document.querySelector('#full-name').parentNode.parentNode.classList.add('has-error');
+
         }else {
-          document.querySelector('[name=full-name]').parentNode.classList.remove('has-error')
+          document.querySelector('#full-name').parentNode.parentNode.classList.remove('has-error');
         }
-        //need to add an error(has-error) class to the ancestor aside tag
+    });
 
-  });
-
-
-
-
-
+//id exp
+document.querySelector('#exp').addEventListener('change', function sliderValue(eventObj){
+      console.log('Im clicking inside the experience range');
+      let exp = document.querySelector('#exp').value + ' years';
+      // document.querySelector('#exp').parentNode.childNodes[1].innertext = exp;
+      document.querySelector('figure').innerText = exp;
+      console.log(exp);
+});
 
 
 
